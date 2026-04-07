cb71891e0264479f040138dd13a41d7211135200
anshy0304 <anshy030405@gmail.com>
2026-04-07 12:19:57 +0530

    Build Registration and Login System

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using backend.Data;
+using backend.DTOs;
+using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
@@ -7,5 +12,51 @@ namespace backend.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly MarketPlaceDbContext _context;
+        public AuthController(MarketPlaceDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto request)
+        {
+            if(await _context.Users.AnyAsync(u => u.Email == request.Email))
+            {
+                return BadRequest("Email is already taken");
+            }
+            var newUser = new User
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Role = request.Role,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password)
+            };
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "Registration Successful"
+            });
+        }
+            [HttpPost("login")]
+            public async Task<ActionResult> Login(LoginDto request)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+                if(user == null)
+                {
+                    return BadRequest("User Not Found");
+                }
+                if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                {
+                    return BadRequest("Wrong Password");
+                }
+                return Ok(new
+                {
+                    Message = "Login successful!",
+                    UserId = user.Id,
+                    Role = user.Role,
+                    Name = user.Name
+                });
+            }
     }
 }
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = "Buyer";
+
+        public string PasswordHash { get; set; } = string.Empty;
     }
 }
