7e09c3fe8d3ecf57a477325dfe92d77d81489957
anshy0304 <anshy030405@gmail.com>
2026-04-11 12:05:57 +0530

    Add Login and Home Page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <h2>MarketPlace Home</h2>
+        <p>Welcome to the best place to buy and sell items!</p>
+    </>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+        <h2>Login Page</h2>
+        <p>This is Where users will log in. </p>
+    </>
+  )
+}
+
+export default Login
