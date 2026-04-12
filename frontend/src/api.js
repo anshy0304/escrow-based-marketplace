c6feb1aa10d9047c61ecc1eab548ee4f2e58c23c
anshy0304 <anshy030405@gmail.com>
2026-04-12 16:37:46 +0530

    Fix Syntax erro in Api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-const baseURL = https://escrow-backend-ansh-gpc9baa2f5aqdma4.centralindia-01.azurewebsites.net;
+const baseURL = "https://escrow-backend-ansh-gpc9baa2f5aqdma4.centralindia-01.azurewebsites.net";
 export default baseURL;
